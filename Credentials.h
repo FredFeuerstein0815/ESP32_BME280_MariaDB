d89dd384afc5c2333f42d95b005b8c8a487fc715
FredFeuerstein0815 <110164290+FredFeuerstein0815@users.noreply.github.com>
2023-02-22 07:57:25 +0100

    Create Credentials.h

---
+++ Credentials.h
@@ -0,0 +1,12 @@
+#ifndef Credentials_h
+#define Credentials_h
+
+uint16_t server_port = 3306;
+
+char ssid[] = "SSID";
+char pass[] = "SSIDPassword";
+
+char user[]         = "sqlUsername";              // MySQL user login username
+char password[]     = "sqlPassword";          // MySQL user login password
+
+#endif    //Credentials_h
