50fc6106b5a46b492ea55dade591137ec23bfd52
FredFeuerstein0815 <110164290+FredFeuerstein0815@users.noreply.github.com>
2023-02-26 07:15:47 +0100

    Update ESP32_BME280_MariaDB_DeepSleep_30_Min.ino

--- ESP32_BME280_MariaDB_DeepSleep_30_Min.ino
+++ ESP32_BME280_MariaDB_DeepSleep_30_Min.ino
@@ -127,6 +127,6 @@ configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1);
   MYSQL_DISPLAY("\r\nGehe in Tiefschlaf...");
   MYSQL_DISPLAY("=======================");
   delay(500);
-  esp_sleep_enable_timer_wakeup(uint64_t(1800000000));
+  esp_sleep_enable_timer_wakeup(uint64_t(1080000000));
   esp_deep_sleep_start();
   }
